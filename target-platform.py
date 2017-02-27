93f1d028ce00046c650d0b50377862d3c15d019e
Patrick Winter <patrick.winter@uni-konstanz.de>
2017-02-27 10:20:14 +0100

    Target platform manager does now run in Python 3.

--- target-platform.py
+++ target-platform.py
@@ -1,27 +1,29 @@
 import argparse
-import urllib2
-import base64
-import HTMLParser
+from html.parser import HTMLParser
 import os
 import shutil
-from progressbar import ProgressBar, SimpleProgress, Bar, ETA
+from progressbar import ProgressBar
+import requests
 
 home = os.path.expanduser('~')
 hiddenDir = home + '/.target-platform/'
-currentDir = hiddenDir + 'current/'
-backupDir = hiddenDir + 'backup/'
+current_dir = hiddenDir + 'current/'
+backup_dir = hiddenDir + 'backup/'
 urls = []
 user = None
 password = None
-remoteDirectory = None
-localDirectory = None
+remote_directory = None
+local_directory = None
 files = None
-ignored_files = set(['.exe','.zip','.gz','.md5','.dmg'])
+ignored_files = {'.exe', '.zip', '.gz', '.md5', '.dmg'}
+
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='Mirror target platform')
-    parser.add_argument('option', type=str, help='update: update the current target platform. restore: restore the previous target platform. clean: remove restore point')
+    parser.add_argument('option', type=str, help='update: update the current target platform. restore: restore the '
+                                                 'previous target platform. clean: remove restore point')
     return parser.parse_args()
+
 
 def load_config():
     global urls, password, user
@@ -30,11 +32,12 @@ def load_config():
     for i in range(len(urls)):
         urls[i] = urls[i].replace('\n', '')
         if not urls[i].endswith('/'):
-            urls[i] = urls[i] + '/'
+            urls[i] += '/'
     with open(hiddenDir + 'account', 'r') as file:
         user, password = file.readlines()
     user = user.replace('\n', '')
     password = password.replace('\n', '')
+
 
 def main():
     option = get_arguments().option
@@ -42,7 +45,7 @@ def main():
     if option == 'update':
         backup()
         try:
-            updateFromUrls()
+            update_from_urls()
         except Exception as e:
             print('Error occured: ' + str(e))
             restore()
@@ -53,85 +56,95 @@ def main():
     else:
         print('Unknown option: ' + option)
 
+
 def backup():
-    if os.path.exists(backupDir):
-        shutil.rmtree(currentDir)
-    elif os.path.exists(currentDir):
-        shutil.move(currentDir, backupDir)
+    if os.path.exists(backup_dir):
+        if os.path.exists(current_dir):
+            shutil.rmtree(current_dir)
+    elif os.path.exists(current_dir):
+        shutil.move(current_dir, backup_dir)
+
 
 def restore():
-    if os.path.exists(backupDir):
-        shutil.rmtree(currentDir)
-        shutil.move(backupDir, currentDir)
+    if os.path.exists(backup_dir):
+        if os.path.exists(current_dir):
+            shutil.rmtree(current_dir)
+        shutil.move(backup_dir, current_dir)
         print('Restored backup')
     else:
         print('No backup available')
 
+
 def clean():
-    if os.path.exists(backupDir):
-        shutil.rmtree(backupDir)
+    if os.path.exists(backup_dir):
+        shutil.rmtree(backup_dir)
         print('Backup removed')
     else:
         print('No backup available')
 
-def updateFromUrls():
-    global remoteDirectory, localDirectory, files
+
+def update_from_urls():
+    global remote_directory, local_directory, files
     for url in urls:
         files = []
-        remoteDirectory = url
-        localDirectory = currentDir + os.path.basename(url[:len(url)-1]) + '/'
-        parseFolder(url)
+        remote_directory = url
+        local_directory = current_dir + os.path.basename(url[:len(url) - 1]) + '/'
+        parse_folder(url)
         print('Downloading ' + url)
-        downloadFiles()
+        download_files()
     print()
 
-def parseFolder(url):
-    html = getHtml(url)
-    folders, files = parseHtml(html)
+
+def parse_folder(url):
+    html = get_html(url)
+    folders, file_names = parse_html(html)
     for folder in folders:
-        parseFolder(url + folder)
+        parse_folder(url + folder)
         pass
-    for file in files:
-        addFile(url + file)
+    for file in file_names:
+        add_file(url + file)
 
-def downloadFiles():
-    bar = ProgressBar(widgets=[SimpleProgress(), ' ', Bar(), ' ', ETA()])
-    bar.maxval = len(files)
-    bar.start()
-    for i, file in enumerate(files):
-        localPath = file.replace(remoteDirectory, localDirectory)
-        folder = os.path.dirname(localPath) + '/'
-        downloadFile(file, folder)
-        bar.update(i + 1)
-    bar.finish()
 
-def downloadFile(url, folder):
+def download_files():
+    global files
+    with ProgressBar(max_value=len(files)) as progress:
+        for i, file in enumerate(files):
+            local_path = file.replace(remote_directory, local_directory)
+            folder = os.path.dirname(local_path) + '/'
+            download_file(file, folder)
+            progress.update(i + 1)
+
+
+def download_file(url, folder):
     if not os.path.exists(folder):
         os.makedirs(folder)
-    request = urllib2.Request(url)
-    base64string = base64.b64encode('%s:%s' % (user, password))
-    request.add_header("Authorization", "Basic %s" % base64string)
-    file = urllib2.urlopen(request)
+    auth = requests.auth.HTTPBasicAuth(user, password)
+    response = requests.get(url, auth=auth)
     with open(folder + os.path.basename(url), 'wb') as local_file:
-        local_file.write(file.read())
+        local_file.write(response.content)
 
-def addFile(file):
+
+def add_file(file):
     global files
     extension = os.path.splitext(file)[1]
-    if not extension in ignored_files:
+    if extension not in ignored_files:
         files.append(file)
 
-def getHtml(url):
-    request = urllib2.Request(url)
-    base64string = base64.b64encode('%s:%s' % (user, password))
-    request.add_header("Authorization", "Basic %s" % base64string)
-    return urllib2.urlopen(request).read()
 
-class FileLinkHTMLParser(HTMLParser.HTMLParser):
+def get_html(url):
+    auth = requests.auth.HTTPBasicAuth(user, password)
+    response = requests.get(url, auth=auth)
+    return response.text
+
+
+class FileLinkHTMLParser(HTMLParser):
+
     def __init__(self):
         self.folders = set()
         self.files = set()
         self.valid = False
+        HTMLParser.__init__(self)
+
     def handle_starttag(self, tag, attrs):
         if tag == 'tr':
             for attr in attrs:
@@ -147,17 +160,21 @@ class FileLinkHTMLParser(HTMLParser.HTMLParser):
                         self.folders.add(attr[1])
                     else:
                         self.files.add(attr[1])
+
     def handle_endtag(self, tag):
         if tag == 'tr':
-            valid = False
+            self.valid = False
+
     def handle_data(self, data):
         pass
 
-def parseHtml(html):
+
+def parse_html(html):
     parser = FileLinkHTMLParser()
     parser.reset()
     parser.feed(html)
     return parser.folders, parser.files
+
 
 if __name__ == '__main__':
     main()
